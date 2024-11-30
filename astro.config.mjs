cb0e9a717d33133f5c27b6b2e7c629185caa5f33
softkern <hi@softkern.com>
2024-12-01 00:07:18 +0800

    feat: integrate unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
-// @ts-check
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    UnoCSS({
+      injectReset: true,
+    }),
+  ],
+})
